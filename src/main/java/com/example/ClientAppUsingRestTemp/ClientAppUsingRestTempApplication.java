8443587e8c651800b8328b72da868548cdf3cffd
AamerKhanPathan <aamerkhanpvt@gmail.com>
2024-04-20 09:14:45 +0530

    v1:b-1234, this is rest application for practice

---
+++ src/main/java/com/example/ClientAppUsingRestTemp/ClientAppUsingRestTempApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClientAppUsingRestTemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAppUsingRestTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAppUsingRestTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ClientAppUsingRestTemp/ClientAppUsingRestTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClientAppUsingRestTemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAppUsingRestTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
